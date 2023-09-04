36c564a67e5eafa9a7cbcbfeed28b8ff7832483c
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-09-05 01:56:55 +0300

    Create NavBar component.
    Also add logo to the navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   // import the grid components for chakra
@@ -10,12 +11,12 @@ function App() {
         // md: '48em',medium
         // lg: '62em',large
         // xl: '80em',
-        base: `"nav " " main"`, // on mobile devices will have nav and main grid
-        lg: `"nav nav" "aside main"`, // on large devices we'll have four grids
+        base: `"nav " " main"`,        // on mobile devices will have nav and main grid
+        lg: `"nav nav" "aside main"`,  // on large devices we'll have four grids
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav" >
+        <NavBar></NavBar>
       </GridItem>
       {/* show is a component where the component inside it will be rendered on lg screens and above */}
       <Show above="lg"> 
