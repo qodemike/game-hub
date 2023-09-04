42598fc238923290b2737b2c483241557187c69c
meekbronsen <mikolskeezy@gmail.com>
2023-09-05 00:48:45 +0300

    Creating responsive layout.
    Using chakra and it's components to build a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
+  // import the grid components for chakra
+  // template Areas to define the grid
   return (
-    <>
-
-    </>
-  )
+    <Grid
+      templateAreas={{
+        // sm: '30em',small
+        // md: '48em',medium
+        // lg: '62em',large
+        // xl: '80em',
+        base: `"nav " " main"`, // on mobile devices will have nav and main grid
+        lg: `"nav nav" "aside main"`, // on large devices we'll have four grids
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      {/* show is a component where the component inside it will be rendered on lg screens and above */}
+      <Show above="lg"> 
+        <GridItem area="aside" bg="violet">
+          sidebar
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="lightblue">
+        Body
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
