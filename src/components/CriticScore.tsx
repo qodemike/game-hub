00a3cab5c105f589c18f2f1dd5bc7c6a5596379b
meekbronsen <127454810+meekbronsen@users.noreply.github.com>
2023-09-22 23:28:37 +0300

    Migrate component styles to modules.

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -8,7 +8,7 @@ const CriticScore = ({ score }: Props) => {
   let color = score > 80 ? "green" : score > 50 ? "yellow" : "";
   return (
     // colorScheme is a color theme for the whole component.
-    <Badge borderRadius="4px" variant={"outline"} colorScheme={color} fontSize={"12px"} >
+    <Badge borderRadius="4px" colorScheme={color} fontSize={"12px"} >
       <Text py="2px" px="3px" fontFamily={"Poppins"}>{score}</Text>
     </Badge>
   );
